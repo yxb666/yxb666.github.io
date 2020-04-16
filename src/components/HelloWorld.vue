d94071e35adb3fa072b9fd43c85125920bf22e62
杨晓博 <14729599096>
2020-04-16 19:30:09 +0800

    提交修正

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,6 +29,13 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <div class="aaa" v-for="(item,index) in list" :key="index">
+      <div class="bbb"></div>
+      <div class="ccc">
+        <div class="ccc_sss" :style="item.svip != ''?'':'padding-top:20px;'" v-if="item.vip">{{item.vip}}</div>
+        <div class="ccc_vvv" :style="item.vip != ''?'':'padding-top:20px;'" v-if="item.svip">{{item.svip}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,6 +44,18 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      list:[
+        {vip:"vip1",svip:"svip1"},
+        {vip:"vip2",svip:"svip2"},
+        {vip:"vip3",svip:""},
+        {vip:"",svip:"svip4"},
+        {vip:"vip5",svip:"svip5"},
+        {vip:"vip6",svip:"svip6"}
+      ]
+    }
   }
 }
 </script>
@@ -56,5 +75,36 @@ li {
 }
 a {
   color: #42b983;
+}
+.aaa{
+  width: 120px;
+  height: 200px;
+  background-color: #ccc;
+  display: inline-block;
+  margin-right: 10px;
+  overflow: hidden;
+  .bbb{
+    width: 100%;
+    height: 80px;
+    background: #000;
+  }
+  .ccc{
+    width: 100%;
+    height: 40px;
+    background-color: pink;
+    margin-top: 20px;
+  .ccc_sss{
+    width: 100%;
+    height: 20px;
+    background:blue;
+    color: #fff;
+  }
+  .ccc_vvv{
+    width: 100%;
+    height: 20px;
+    background:yellow;
+    // color: #fff;
+  }
+  }
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const target = "https://yxb666.github.io/";
+module.exports = {
+    lintOnSave:false,
+    devServer:{
+        port : 9999
+    }
+}
