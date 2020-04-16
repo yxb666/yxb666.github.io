b1fd45e6e9c1635bceeb4c0becdff1273744bcd3
杨晓博 <14729599096>
2020-04-16 20:31:23 +0800

    添加配置，打包后本地访问

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const target = "https://yxb666.github.io/";
 module.exports = {
     lintOnSave:false,
+    publicPath: './',
     devServer:{
         port : 9999
     }
